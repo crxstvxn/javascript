95f372ba68dca51a552fb42e4b2db848429f0785
Juan Cristian <crxstvxn@hotmail.com>
2023-10-31 14:26:17 -0300

    estudando js

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 15
+console.log(`A velocidade do seu carro é ${vel} KM/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permita. MULTADO!`)
+}
+console.log(`Dirija sempre com cinto de segurança!`)
