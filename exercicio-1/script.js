673740973f649504657ed8265e6922c11eb069ff
Juan Cristian <crxstvxn@hotmail.com>
2024-01-22 21:42:25 -0300

    estudando 2

--- exercicio-1/script.js
+++ exercicio-1/script.js
@@ -1,7 +1,18 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
-    var msg = window.document.getElementById('imagem')
+    var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.InnerHTML = `Agora sao ${hora} horas.`
+    msg.innerHTML = `Agora sao ${hora} horas.`
+
+    if (hora >=0 && hora <12){
+        img.src = 'manha.png'
+        document.body.style.background = 'green'
+    } else if (hora >12 && hora <=18) {
+        img.src = 'tarde.png'
+        document.body.style.background = 'yellow'
+    } else {
+        img.src = 'noite.png'
+        document.body.style.background = 'black'
+    }
 }
